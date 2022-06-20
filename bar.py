f9ee25e77d651e90ca4f2da4aa518ae8bb3ec85f
Ihor Mytlushko <61839115+cur1osityay@users.noreply.github.com>
2022-06-20 15:36:42 +0300

    Create bar.py

---
+++ bar.py
@@ -0,0 +1,20 @@
+import time
+import sys
+toolbar_width = 90
+
+start_time = time.time()
+
+def toolbar1():
+    print('EXEC TIME IS 90S)')
+    sys.stdout.write("[%s]" % (" " * toolbar_width))
+    sys.stdout.flush()
+    sys.stdout.write("\b" * (toolbar_width+1)) 
+    for i in range(toolbar_width):
+        time.sleep(1) 
+        sys.stdout.write("■")
+        sys.stdout.flush()
+    sys.stdout.write("]\n") 
+
+toolbar1()
+
+print("--- %s seconds ---" % (time.time() - start_time))
